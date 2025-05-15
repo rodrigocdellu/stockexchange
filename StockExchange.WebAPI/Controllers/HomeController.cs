37f558ca258c23138388576827ab8670d1fb32b3
Rodrigo C. Dellu <rodrigocdellu.trabalho@outlook.com>
2025-05-15 02:05:47 -0300

    CSS colors adjusted.

--- StockExchange.WebAPI/Controllers/HomeController.cs
+++ StockExchange.WebAPI/Controllers/HomeController.cs
@@ -36,8 +36,8 @@ public sealed class HomeController : ControllerBase
                         :root {{
                             --primary-white: #ffffff;          /* Mantém branco */
                             --secondary-white: #ede7f6;        /* Deep Purple 50 - Fundo lavanda bem claro */
-                            --primary-red: #5a2c8f;            /* Tom central personalizado - Base roxa desejada */
-                            --secondary-red: #7e57c2;          /* Deep Purple 400 - Um pouco mais claro e vibrante */
+                            --primary-color: #5a2c8f;            /* Tom central personalizado - Base roxa desejada */
+                            --secondary-color: #7e57c2;          /* Deep Purple 400 - Um pouco mais claro e vibrante */
                             --primary-black: #311b53;          /* Deep Purple 900 - Roxo muito escuro */
                             --font-color: #333333;             /* Cinza - Tom da fonte */
                         }}
