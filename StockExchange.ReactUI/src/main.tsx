9d035cc2f1d41c53c822f829892a86fe7e752a38
Rodrigo C. Dellu <rodrigocdellu.trabalho@outlook.com>
2025-05-15 01:07:46 -0300

    Adjusting folders to compare with AngularUI.

--- StockExchange.ReactUI/src/main.tsx
+++ StockExchange.ReactUI/src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '../styles.css'
+import '/styles.css'
+import Solicitacao from './app/components/solicitacao/solicitacao.component'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Solicitacao></Solicitacao>
   </StrictMode>,
 )
--- StockExchange.ReactUI/vite.config.ts
+++ StockExchange.ReactUI/vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as Path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    envDir: __dirname,
+    publicDir: Path.join(__dirname, "public"),
+    root: Path.join(__dirname, "src"),
+    build: {
+        outDir: Path.join(__dirname, "dist"),
+        rollupOptions: {
+            input: [Path.join(__dirname, "src/index.html")],
+        },
+    },
+    plugins: [
+        react(),
+    ],
+    resolve: {
+        alias: {
+            "@": Path.resolve(__dirname, "./src"),
+        },
+    },
+    //plugins: [react()], // 2025/05/14 - It started with just this line
 })
