c5f00f1b25c6728322de5353ed1f4abe1c7ea404
Rodrigo C. Dellu <rodrigocdellu.trabalho@outlook.com>
2025-05-26 20:01:18 -0300

    Adjusted form style.

--- StockExchange.ReactUI/src/app/components/solicitacao/Solicitacao.tsx
+++ StockExchange.ReactUI/src/app/components/solicitacao/Solicitacao.tsx
@@ -1,13 +1,58 @@
 import type { Retorno } from '../../models/Retorno'
 import styles from './Solicitacao.module.css'
+import { useForm } from 'react-hook-form'
+import { TextField, Button } from '@mui/material';
 
 export default function Solicitacao({ retorno }: Retorno) {
+    const { register, handleSubmit, formState: { errors }, reset } = useForm<FormData>();
+
+    type FormData = {
+        investimento: number;
+        meses: number;
+    };
+
+    const onSubmit = (data: FormData) => {
+        console.log(data);
+        // fazer chamada para API etc.
+    };
+
     return (
         <div className={styles.card}>
-            <h1>Bem-vindo(a) à Aplicação React CDB</h1>
+            <h1>Bem-vindo(a) à Aplicação<br />React CDB</h1>
             <div className={styles.internalCard}>
                 <h2>Solicitar o Calculo do Investimento CDB</h2>
-                <h3>###FORMULÁRIO AQUI###</h3>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <TextField
+                        label="Investimento*"
+                        variant="outlined"
+                        fullWidth
+                        margin="normal"
+                        {...register("investimento", { required: "Campo obrigatório" })}
+                        error={!!errors.investimento}
+                        helperText={errors.investimento?.message}
+                        className={styles.textField}
+                    />
+
+                    <TextField
+                        label="Meses*"
+                        variant="outlined"
+                        type="number"
+                        fullWidth
+                        margin="normal"
+                        {...register("meses", {
+                        required: "Campo obrigatório",
+                        min: { value: 1, message: "Mínimo de 1 mês" },
+                        max: { value: 1200, message: "Máximo de 1200 meses" }
+                        })}
+                        error={!!errors.meses}
+                        helperText={errors.meses?.message}
+                        className={styles.textField}
+                    />
+                    
+                    <Button className={styles.buttonSpaceLeft} variant="outlined" color="primary" type="submit">Solicitar</Button>
+                    
+                    <Button className={styles.buttonSpaceRight} variant="outlined" color="primary" onClick={() => reset()}>Limpar</Button>
+                </form>
             </div>
             <div className={styles.internalCard}>
                 <h2>Resultado do Investimento CDB</h2>
