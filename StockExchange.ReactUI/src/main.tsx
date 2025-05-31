a9b579a623e5a17f5e59a029ffc8945b94f246a3
Rodrigo C. Dellu <rodrigocdellu.trabalho@outlook.com>
2025-05-31 00:16:50 -0300

    SonarQube - Do not import modules using an absolute path.

--- StockExchange.ReactUI/src/main.tsx
+++ StockExchange.ReactUI/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '/styles.css'
+import './styles.css'
 import SolicitacaoComponent from './app/components/solicitacao/SolicitacaoComponent'
 
 createRoot(document.getElementById('root')!).render(
