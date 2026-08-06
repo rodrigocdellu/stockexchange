cf04e03e52ab0a2531f7bb8c0e83e00e1183c93c
rodrigocdellu <rodrigocdellu.trabalho@outlook.com>
2026-08-05 21:06:10 -0300

    Fix for unit tests.

--- StockExchange.WebAPI/Helpers/DateTimeHelper.cs
+++ StockExchange.WebAPI/Helpers/DateTimeHelper.cs
@@ -17,7 +17,7 @@ public class SystemTimeZoneProvider : ITimeZoneProvider
 
 public static class DateTimeHelper
 {
-    private static TimeZoneInfo? GetBrasilianTimeZone(SystemTimeZoneProvider provider)
+    private static TimeZoneInfo? GetBrasilianTimeZone(ITimeZoneProvider provider)
     {
         try
         {
